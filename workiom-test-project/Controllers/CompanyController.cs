14a1218f92f860320e8f5d7bfb2b3e1d06d5b61d
Tarık Haşimoğlu <tarik.hasimoglu@advancity.com.tr>
2020-08-15 08:25:56 +0300

    Add project files.

---
+++ workiom-test-project/Controllers/CompanyController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace workiom_test_project.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CompanyController : ControllerBase
+    {
+        private readonly ILogger<CompanyController> _logger;
+
+        public CompanyController(ILogger<CompanyController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public string Get()
+        {
+            return "GET";
+        }
+    }
+}
