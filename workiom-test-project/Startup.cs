f45105dfe04450b13e45d7c4b9573ab4a2ea32eb
Tarık Haşimoğlu <tarik.hasimoglu@advancity.com.tr>
2020-08-15 16:16:49 +0300

    Add Version Info

--- workiom-test-project/Startup.cs
+++ workiom-test-project/Startup.cs
@@ -1,15 +1,19 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using workiom_test_project.Data;
+using workiom_test_project.Data.Interfaces;
 using workiom_test_project.Models;
 
 namespace workiom_test_project
@@ -27,11 +31,9 @@ namespace workiom_test_project
         public void ConfigureServices(IServiceCollection services)
         {
             // requires using Microsoft.Extensions.Options
-            services.Configure<DbSettings>(
-                Configuration.GetSection(nameof(DbSettings)));
-
-            services.AddSingleton<DbSettings>(sp =>
-                sp.GetRequiredService<IOptions<DbSettings>>().Value);
+            services.Configure<DbSettings>(Configuration.GetSection(nameof(DbSettings)));
+            services.AddSingleton<IDbSettings>(sp => sp.GetRequiredService<IOptions<DbSettings>>().Value);
+            services.AddSingleton<IDb, Db>();
             services.AddControllers();
         }
 
@@ -50,6 +52,19 @@ namespace workiom_test_project
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
+            });
+
+            app.Run(async (context) =>
+            {
+                if (context.Request.Path == "/" || context.Request.Path == "/version")
+                {
+                    var version = System.Reflection.Assembly.GetEntryAssembly().GetCustomAttribute<AssemblyInformationalVersionAttribute>().InformationalVersion;
+                    await context.Response.WriteAsync("Workiom Test Project 2020 API v" + version);
+                }
+                else
+                {
+                    context.Response.StatusCode = 404;
+                }
             });
         }
     }
